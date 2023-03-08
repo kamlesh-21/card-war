001380c37544196a67f525d9013ac3780abfe34c
kamlesh-21 <kumar2105k@gmail.com>
2023-03-09 00:50:55 +0530

    updated variable name
    had an error due to usage of capital letter at incorrect place

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-const deckId = ""
+let deckId
 
 function handleClick(){
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res => res.json())
     .then(data => {
-        deckId = data.deckId
+        deckId = data.deck_id
     })
 }
 
 document.getElementById("new-card").addEventListener("click", handleClick)
-document.getAnimations("draw-cards").addEventListener("click", gettwoCards)
 
-function gettwoCards(){
-    if(deckId){
+document.getElementById("draw-cards").addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res=>res.json())
         .then(data=>{
             document.getElementById("cards").innerHTML = `
                 <img src=${data.cards[0].image}>
-                <img src="${data.cards[1].image}">
+                <img src=${data.cards[1].image}>
             `
         })
-    }else {
-        alert("get New Card, first")
-    }
-}
+})

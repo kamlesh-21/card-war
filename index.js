145e7740c8b06dc407dec3ba96539193cf1c5b6a
kamlesh-21 <kumar2105k@gmail.com>
2023-03-08 23:57:11 +0530

    updated new card and new deck functions

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const newCard = document.getElementById("new-card")
+const deckId
 
-newCard.addEventListener("click", ()=>{
+function handleClick(){
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res => res.json())
-    .then(data => console.log(data))
-})
+    .then(data => {
+        deckId = data.deckId
+    })
+}
+
+document.getElementById("new-card").addEventListener("click", handleClick)
+document.getAnimations("draw-cards").addEventListener("click", gettwoCards)
+
+function gettwoCards(){
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
+}
